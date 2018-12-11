88968dabc2272b46c064773bed0da1d121bf2af6
krowitz <44564395+krowitz@users.noreply.github.com>
2018-12-11 21:47:45 +0800

    Documentation for IllusGameServer

--- IllusGameServer.java
+++ IllusGameServer.java
@@ -1,4 +1,8 @@
 /**
+ * The Illus Game Server class provides all necessary methods and mechanisms to host a game of Illus.
+ * It is responsible for maintaining game state and forwarding the game to its designed game flow.
+ * It keeps track of all the players and scoring.
+ *
  * 1. Create Lobby
  * 2. Wait for players to join
  * 3. Players will be assigned an id by the chat server.
@@ -57,6 +61,14 @@ public class IllusGameServer implements Runnable {
     private Dictionary dictionary;
     private String current_word;
 
+    /**
+     * This is the constructor for IllusGameServer. It creates a chat lobby on the provided game server, initializes the
+     * dictionary class, sets up the max players and sets up a Datagram Socket to accept udp connections.
+     * @param serverName ip address of provided chat server
+     * @param port port of said chat server
+     * @param maxPlayers an integer between 3 to 5 which sets up the maximum number of players supported
+     * @throws Exception
+     */
     public IllusGameServer(String serverName, int port, int maxPlayers) throws Exception {
         Socket server = new Socket();
         server.connect(new InetSocketAddress(serverName, port), 10000);
@@ -69,6 +81,12 @@ public class IllusGameServer implements Runnable {
         this.dictionary = new Dictionary();
     }
 
+    /**
+     *  This method is used to create a chat lobby that holds the number of maximum players
+     * @param maxPlayers The maximum number of players supported.
+     * @return
+     * @throws Exception
+     */
     private String createLobby(int maxPlayers) throws Exception {
         CreateLobbyPacket createLobbyPacket = CreateLobbyPacket.newBuilder().setType(PacketType.CREATE_LOBBY)
                 .setMaxPlayers(maxPlayers).build();
@@ -98,6 +116,11 @@ public class IllusGameServer implements Runnable {
         return lobbyId;
     }
 
+    /**
+     * This method is used by the server to send a message to a specific player.
+     * @param player The player who is the recipient of the message
+     * @param msg The message to be sent
+     */
     public void send(IllusPlayer player, String msg) {
         DatagramPacket packet;
         byte buf[] = msg.getBytes();
@@ -109,12 +132,21 @@ public class IllusGameServer implements Runnable {
         }
     }
 
+    /**
+     * This method is used to send a message to all players.
+     * @param msg The message to be sent
+     */
     public void sendToAll(String msg) {
         for (IllusPlayer player : players) {
             send(player, msg);
         }
     }
 
+    /**
+     * This method is created to get players by name. It is unused.
+     * @param name The name of the player.
+     * @return
+     */
     public IllusPlayer getPlayerByName(String name) {
         IllusPlayer player = null;
         for (IllusPlayer temp : players) {
@@ -126,17 +158,27 @@ public class IllusGameServer implements Runnable {
         return player;
     }
 
+    /**
+     * This method is used to set the current word to be guessed on a specific round
+     */
     private void setCurrentWord(){
         this.current_word = dictionary.getAnswer();
     }
 
+    /**
+     * This method is used to start the round after the countdown
+     */
     public void start_after_countdown(){
         sendToAll("WORD "+dictionary.getAnswer());
         timer = new Time(60, true);
         timer.startTimer();
-        
+
         sendToAll("START");
     }
+
+    /**
+     * This class is a customized timer for the game.
+     */
     // TIMER
     public class Time {
         Timer timer;
@@ -154,7 +196,7 @@ public class IllusGameServer implements Runnable {
         public void setTimerStatus(){
             this.isInGame = !this.isInGame;
         }
-        
+
         public Boolean getTimerStatus(){
             return this.isInGame;
         }
@@ -166,7 +208,7 @@ public class IllusGameServer implements Runnable {
             if(time == 0 && !isInGame){
                 start_after_countdown();
             }
-            
+
         }
 
         public int getTime() {
@@ -190,6 +232,10 @@ public class IllusGameServer implements Runnable {
         }
     }
 
+    /**
+     * main method
+     * @param args
+     */
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -224,13 +270,16 @@ public class IllusGameServer implements Runnable {
 
     }
 
+    /**
+     * This method will run throughout the game session. This facilitates the entire game play
+     */
     @Override
     public void run() {
-        int loadedUI = 0;
-        level = 0;
-        IllusPlayer drawer = null;
-        int timeUp = 0;
-        while (true) {
+        int loadedUI = 0; //tracks how many players have their UIs loaded
+        level = 0; //starting level
+        IllusPlayer drawer = null; //every round, the value will be the player assigned to draw
+        int timeUp = 0; //tracks how many players sent the time up message
+        while (true) { //run infinitely
 
             byte[] buf = new byte[256];
             DatagramPacket packet = new DatagramPacket(buf, buf.length);
@@ -251,6 +300,8 @@ public class IllusGameServer implements Runnable {
             String[] dataArray;
 
             switch (state) {
+                //This state is the state of the game for when players are about to connect.
+                //At this state, a connection request will be responded with the chat server lobby ID
                 case "WAITING":
                     if (data.startsWith("CONNECT")) {
                         String connectingPlayerName = "";
@@ -276,16 +327,19 @@ public class IllusGameServer implements Runnable {
                         }
                     }
                     break;
+                    //This state is the initial round of the game. Note that the code here is repeated on different
+                    //flavors inside the run case to facilitate the initialization of a new game round.
                 case "INIT_LEVEL":
                     dataArray = data.split(" ");
 
+                    //counts how many players has loaded the UI. If all players are connected, start the game.
                     if (data.startsWith("START")) {
                         loadedUI++;
 
                         if (loadedUI == maxPlayers) {
 
                             state = "RUNNING";
-                            drawer = players.get(level % maxPlayers);
+                            drawer = players.get(level % maxPlayers); //determine which player will draw
                             System.out.println("\nPLAYER " + drawer.getName() + " will draw for this level");
                             sendToAll("DRAWER " + drawer.getName() + " ");
                             level++;
@@ -299,15 +353,16 @@ public class IllusGameServer implements Runnable {
                         }
                     }
                     break;
+                    //This state is the stage of the game where rounds take place.
                 case "RUNNING":
                     dataArray = data.split(" ");
-                    if (data.startsWith("CLEAR")) {
+                    if (data.startsWith("CLEAR")) { //clear the paint screen of all players
                         sendToAll("CLEAR CANVAS");
                     } else if (data.startsWith("PAINT")) {
 
                         sendToAll("PAINT " + dataArray[1] + " " + dataArray[2] + " " + dataArray[3]);
 
-                    } else if (data.startsWith("ANSWER")) {
+                    } else if (data.startsWith("ANSWER")) { //validate a player's answer
                         String playerName = dataArray[1].trim();
                         String guess = dataArray[2].trim();
 
@@ -315,28 +370,29 @@ public class IllusGameServer implements Runnable {
 
                         //check answer here
                         if (dictionary.validateWord(guess)) {
-                            
+
                             for(IllusPlayer player : players){
                                 if(player.getName().equals(playerName)){
                                     // increment player score
-                                        player.setScore(player.getScore() + timer.getTime());
+                                    player.setScore(player.getScore() + timer.getTime());
                                 }
                             }
                             sendToAll("CORRECT_ANSWER " + playerName + " " + dictionary.getAnswer());
-                            
+
                             updateScores();
                             timeUp++;
                             correctAnswers++;
-                            
-                           
-                            // end of level
+
+
+                            // end of level (1) where all players are able to guess the word.
+                            //if all players have guessed the word, give bonus points to drawer and start a new round.
                             if(correctAnswers == (maxPlayers-1)){
                                 for(IllusPlayer player : players){
                                     // drawer earns points
                                     if(player.getName().equals(drawer.getName())){
                                         player.setScore(player.getScore() + 10);
                                     }
-                                    
+
                                 }
 
                                 timer.stopTimer();
@@ -357,7 +413,9 @@ public class IllusGameServer implements Runnable {
                                 }
                             }
                         }
-                    } else if (data.startsWith("CHOICE")) {
+                    }
+                    //Drawer chooses a word to draw, the timer should start a countdown, then count.
+                    else if (data.startsWith("CHOICE")) {
                         //drawer finalizes word to guess
                         String choice = dataArray[1].trim();
                         System.out.println("\n[!] Drawer chose " + choice);
@@ -375,14 +433,18 @@ public class IllusGameServer implements Runnable {
                         sendToAll("CLEAR CANVAS");
                         timer = new Time(4);
                         timer.startTimer();
-   
+
                     }
+                    //When the time runs out, reveal  the word.
                     else if(data.startsWith("TIME_UP")){
                         System.out.println("time up");
                         for(IllusPlayer player : players){
                             send(player, "REVEAL " + current_word);
                         }
                         timeUp++;
+
+                        //end level 2 when not all players are able to guess the word
+                        //if no one guessed the word correctly, penalize the drawer
                         if(timeUp == maxPlayers){
                             if(correctAnswers == 0){
                                 for(IllusPlayer player : players){
@@ -395,11 +457,13 @@ public class IllusGameServer implements Runnable {
                             level++;
                             correctAnswers = 0;
                             timeUp = 0;
+
+                            //first if means the last round has been played
                             if(level >= max_level-1){
                                 System.out.println("END GAME TIME UP");
                                 for(IllusPlayer player : players)
-                                        send(player, "END_GAME " + player.getScore());
-                            }else{ 
+                                    send(player, "END_GAME " + player.getScore());
+                            }else{
                                 System.out.println("\nPLAYER " + drawer.getName() + " will draw for level (" + level + "/" + max_level + ")");
                                 sendToAll("DRAWER " + drawer.getName() + " ");
                                 dictionary = new Dictionary();
@@ -416,6 +480,9 @@ public class IllusGameServer implements Runnable {
 
     }
 
+    /**
+     * This method is used to update the clients on the scores of all players.
+     */
     private void updateScores(){
         StringBuilder sb = new StringBuilder("SCORES ");
         for(int i = 0; i < players.size(); i++){
